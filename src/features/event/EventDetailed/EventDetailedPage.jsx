3ad5a390ebf51c15ffb7a0744c3bfd767f97af0c
Moon <muunuu5555@gmail.com>
2020-04-07 17:46:17 +0800

    End of Section 6

---
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const EventDetailedPage = () => {
+  return <div>EventDetailedPage</div>;
+};
+export default EventDetailedPage;
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,26 +1,51 @@
-import React, { Component } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "./Menus/SignedOutMenu";
+import SignedInMenu from "./Menus/SignedInMenu";
 
- class NavBar extends Component {
-    render() {
-        return (
-               <Menu inverted fixed="top">
-                 <Container>
-                   <Menu.Item header>
-                     <img src="assets/logo.png" alt="logo" />
-                     Re-vents
-                   </Menu.Item>
-                   <Menu.Item name="Арга хэмжээ" />
-                   <Menu.Item>
-                     <Button floated="right" positive inverted content="Арга хэмжээ үүсгэх" />
-                   </Menu.Item>
-                   <Menu.Item position="right">
-                     <Button basic inverted content="Нэвтрэх" />
-                     <Button basic inverted content="Гарах" style={{marginLeft: '0.5em'}} />
-                   </Menu.Item>
-                 </Container>
-               </Menu>
-        )
-    }
+class NavBar extends Component {
+  state = {
+    authenticated: true,
+  };
+
+  handleSignIn = () => {
+    this.setState({ authenticated: true });
+  };
+  handleSignOut = () => {
+    this.setState({ authenticated: false });
+    this.props.history.push("/");
+  };
+
+  render() {
+    const { authenticated } = this.state;
+    return (
+      <Menu inverted fixed='top'>
+        <Container>
+          <Menu.Item as={NavLink} exact to='/' header>
+            <img src='assets/logo.png' alt='logo' />
+            Re-vents
+          </Menu.Item>
+          <Menu.Item as={NavLink} to='/events' name='Арга хэмжээ' />
+          <Menu.Item as={NavLink} to='/people' name='Хүн' />
+          <Menu.Item>
+            <Button
+              as={Link}
+              to='/createEvent'
+              floated='right'
+              positive
+              inverted
+              content='Арга хэмжээ үүсгэх'
+            />
+          </Menu.Item>
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
+        </Container>
+      </Menu>
+    );
+  }
 }
-export default NavBar
+export default withRouter(NavBar);
